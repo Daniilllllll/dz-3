1da0e4a3c03255a314865c7a80c2b68c1ef62c61
Daniilllllll <privetdanilka22@gmail.com>
2024-09-22 17:59:02 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-DZ = "12"
-Time = "1.5"
+DZ = 12
+Time = 1.5
 Name = "python"
-Time_one_task = 1.5/12
+Time_one_task = (Time)/(DZ)
 print(('Курс:' ' ' f'{Name}' ',') , 'всего задач:' ' ' f'{DZ}' ',' , "затраченно часов:" ' ' f'{Time}' ',' , "среднее время выполнения" ' ' f"{Time_one_task}" ' ''часа')
